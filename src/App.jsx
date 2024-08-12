950525e6eac6005fee6393521c776854dd316736
Carolina <c.floresvaldivia@hotmail.com>
2024-08-11 20:33:15 -0400

    login y register page importadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import NavbarPizza from './components/Navbar';
-import Home from './components/Home';
+// import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
+// import RegisterPage from './components/RegisterPage';
+import LoginPage from './components/LoginPage';
 
 
 function App() {
   return (
     <div>
       <NavbarPizza />
-      <Home />
+      {/* <Home /> */}
+      {/* <RegisterPage /> */}
+      <LoginPage/>
       <Footer />
     </div>
   );

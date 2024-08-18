663cacb697f07933bb748385a397a1e90bbb76d1
Carolina <c.floresvaldivia@hotmail.com>
2024-08-18 12:33:22 -0400

    carrito y 6  cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import NavbarPizza from './components/Navbar';
+import Cart from './components/Cart'
 // import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
 // import RegisterPage from './components/RegisterPage';
-import LoginPage from './components/LoginPage';
+// import LoginPage from './components/LoginPage';
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
       <NavbarPizza />
       {/* <Home /> */}
       {/* <RegisterPage /> */}
-      <LoginPage/>
+      {/* <LoginPage/> */}
+      <Cart/>
       <Footer />
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,41 +3,26 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Header from './Header';
 import CardPizza from './CardPizza';
+import { pizzas } from '../assets/JS/pizzas.js'
 
-function Home() {
-    return (
-      <div>
-        <Header />
-        <Container>
-          <Row>
-            <Col md={4} className="mb-4 d-flex flex-column" >
-              <CardPizza
-                name="Napolitana"
-                price={5950}
-                ingredients={["mozzarella", "tomates", "jamón", "orégano"]}
-                img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"
-              />
-            </Col>
-            <Col md={4} className="mb-4 d-flex flex-column" >
-              <CardPizza
-                name="Española"
-                price={6950}
-                ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]}
-                img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab"
-              />
-            </Col>
-            <Col md={4} className="mb-4 d-flex flex-column" >
-              <CardPizza
-                name="Pepperoni"
-                price={6950}
-                ingredients={["mozzarella", "pepperoni", "orégano"]}
-                img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"
-              />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
-  
-  export default Home;
+const Home = () => {
+  return (
+    <Container>
+      <Row>
+        {pizzas.map((pizza, index) => (
+          <Col key={pizza.id} sm={12} md={6} lg={4} className="mb-4">
+            <CardPizza
+              name={pizza.name}
+              desc={pizza.desc}
+              img={pizza.img}
+              ingredients={pizza.ingredients}
+              price={pizza.price}
+            />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
